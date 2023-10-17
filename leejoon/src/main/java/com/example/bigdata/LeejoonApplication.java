01cb42ecdabe0232b8eacca3d0cbde2966a212af
leejooooooon <dlwns7293@naver.com>
2023-10-17 09:53:11 +0900

    테스트입니다

---
+++ leejoon/src/main/java/com/example/bigdata/LeejoonApplication.java
@@ -0,0 +1,14 @@
+package com.example.bigdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeejoonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeejoonApplication.class, args);
+	}
+
+}
+
