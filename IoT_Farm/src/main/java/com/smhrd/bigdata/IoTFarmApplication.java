dd8bc248dae6961a5ff9f98602d7d657b516bf16
usernamekimdaeun <daeun1505@naver.com>
2023-10-17 09:37:09 +0900

    111

---
+++ IoT_Farm/src/main/java/com/smhrd/bigdata/IoTFarmApplication.java
@@ -0,0 +1,13 @@
+package com.smhrd.bigdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoTFarmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IoTFarmApplication.class, args);
+	}
+
+}
